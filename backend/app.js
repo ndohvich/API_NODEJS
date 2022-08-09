b9f38b579e7ed1fefc30823ca8dafbe8050120cd
NDOH NDOH Jules Moise Yannick <ndohmoise@gmail.com>
2022-08-09 17:25:10 +0100

    création du backend

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const stuffRoutes = require('./routes/stuff');
+const userROutes = require('./routes/user');
+
+//Ajoute de la base de données avec mongoose
+mongoose.connect('mongodb+srv://ndohvich:171191Yannickndohjules@cluster0.zxie4.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+//me sert de controler les fichiers en tete
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userROutes);
+
+module.exports = app;
